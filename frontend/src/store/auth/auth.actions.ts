984a677579ea937ee8c3216f85c30694c3ffaa40
PovilasB <povilas.baranovas@gmail.com>
2019-01-22 17:57:06 +0200

    TSlint fixes

--- frontend/src/store/auth/auth.actions.ts
+++ frontend/src/store/auth/auth.actions.ts
@@ -6,4 +6,3 @@ const actionCreator = typescriptFsa();
 export const login = actionCreator.async<LoginActionPayload, UserAuth>('LOGIN');
 export const logout = actionCreator.async<{}, {}>('LOGOUT');
 export const getUserData = actionCreator.async<{}, UserData>('GET_USER_DATA');
-
