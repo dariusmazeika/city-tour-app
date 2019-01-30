3125f2d127a21a7463491dc06fe0269655260500
Gediminas Ubartas <gedisu@gmail.com>
2019-01-30 22:14:15 +0200

    Assets fix

--- backend/context_processors.py
+++ backend/context_processors.py
@@ -8,12 +8,15 @@ from os import path
 from django.conf import settings
 
 STATIC_ASSETS = None
+STATIC_VENDOR_ASSETS = None
 
 if not settings.WEBPACK_DEV_SERVER:
     ASSETS_JSON_FNAME = path.join(settings.STATIC_ASSETS_JSON)
     try:
         with open(ASSETS_JSON_FNAME, 'r') as f:
-            STATIC_ASSETS = json.load(f)['main']
+            json_data = json.load(f)
+            STATIC_ASSETS = json_data['main']
+            STATIC_VENDOR_ASSETS = json_data['vendors~main']
     except Exception:
         raise Exception('failed to read %s. was frontend built?' % ASSETS_JSON_FNAME)
 
@@ -23,5 +26,6 @@ def static_resources(request):
     """ used by base.html to figure out what statics to load """
     return {
         "WEBPACK_DEV_SERVER": settings.WEBPACK_DEV_SERVER,
-        "static_assets": STATIC_ASSETS
+        "static_assets": STATIC_ASSETS,
+        "static_vendor_assets": STATIC_VENDOR_ASSETS,
     }
