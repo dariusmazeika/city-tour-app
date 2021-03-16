4d6fb1fdb289438672fb8ac81295849d35f2bba6
Albertas Gimbutas <albertasgim@gmail.com>
2021-03-16 09:08:01 +0200

    Treat warning as errors
    Do not show coverage on test fail.
    Fix deprecation warning.

--- backend/bin/check.sh
+++ backend/bin/check.sh
@@ -12,7 +12,7 @@ echo "2. MyPy"
 python3 -m mypy apps --config-file mypy.ini
 
 echo "3. Tests"
-python3 -m pytest apps --cov --exitfirst
+python3 -m pytest apps --cov --exitfirst --no-cov-on-fail
 
 echo ""
 echo "ALL CHECKS PASSED"
