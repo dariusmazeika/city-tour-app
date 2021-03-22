6b67987bb2ef3862ce6fac60ae48d7a14db28ba1
Tomas Varzgalys <tomasv@cornercasetech.com>
2021-03-22 12:51:46 +0200

    fix translations serialization in manifest API

--- backend/apps/manifests/generator.py
+++ backend/apps/manifests/generator.py
@@ -5,7 +5,7 @@ from apps.translations.models import Language, Translation
 
 
 def get_translations(language):
-    return {trans.message.message_id: trans.title for trans in Translation.objects.filter(language=language)}
+    return {trans.message.message_id: trans.text for trans in Translation.objects.filter(language=language)}
 
 
 def generate_messages():
@@ -20,21 +20,6 @@ def get_enabled_languages(config):
             'name': lang.name
         } for lang in config.enabled_languages.all()
     ]
-
-
-def get_page_translations(page, add_content=False):
-    if page is None:
-        return {}
-    translations = {}
-    icon = page.icon.url if page.icon else None
-    for translation in page.page_translations.all():
-        translations[translation.language.pk] = {
-            'slug': translation.slug,
-            'title': translation.title,
-            'icon': icon,
-            'content': translation.title if add_content else None
-        }
-    return translations
 
 
 def generate_config():
