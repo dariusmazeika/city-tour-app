df27f3f09ab8585368fd269a93b6e18bed8a881d
Tautvydas Musteikis <tautvydas@cornercasetech.com>
2021-03-23 02:48:10 +0000

    Port from mindtales

--- backend/apps/utils/storage.py
+++ backend/apps/utils/storage.py
@@ -1,18 +1,16 @@
-import os
-
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class MediaStorage(S3Boto3Storage):
-    bucket_name = os.getenv("STORAGE_BUCKET_NAME")
+    bucket_name = settings.STORAGE_BUCKET_NAME  # type: ignore
     location = "media"
     file_overwrite = True
     custom_domain = f"{bucket_name}.s3.{settings.AWS_DEFAULT_REGION}.amazonaws.com"  # type: ignore
 
 
 class StaticStorage(S3Boto3Storage):
-    bucket_name = os.getenv("STATIC_BUCKET_NAME")
+    bucket_name = settings.STATIC_BUCKET_NAME  # type: ignore
     location = "static"
     file_overwrite = True
     custom_domain = f"{bucket_name}.s3.{settings.AWS_DEFAULT_REGION}.amazonaws.com"  # type: ignore
