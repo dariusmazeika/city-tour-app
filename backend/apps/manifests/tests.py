4b140bdea275cf635c054a8786f4bf8a7212c3d7
Dziugas Tornau <dziugas@cornercasetech.com>
2021-06-04 17:52:18 +0300

    add manifest and email tests

--- backend/apps/manifests/tests.py
+++ backend/apps/manifests/tests.py
@@ -17,3 +17,38 @@ class AppConfigTestCase(BaseTestCase):
         self.assertEqual(response.json()['enabled_languages'], [])
         self.assertEqual(response.json()['translations']['lt'], {})
         self.assertEqual(response.json()['translations']['en'], {})
+
+    def test_regenerate_manifest(self):
+        url = reverse("regenerate-cache")
+        response = self.client.get(url, format="json", HTTP_REFERER="test/url")
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+        self.assertEqual(response.url, "test/url")
+
+    def test_regenerate_manifest_redirects(self):
+        site_config = SiteConfiguration.get_solo()
+        url = reverse("regenerate-cache")
+        url_final = reverse("app-config")
+
+        response = self.client.get(url, format="json", HTTP_REFERER=url_final, follow=True)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json()["version"], site_config.manifest_version)
+        self.assertEqual(response.json()["default_language"], settings.DEFAULT_LANGUAGE)
+        self.assertEqual(response.json()["enabled_languages"], [])
+        self.assertEqual(response.json()["translations"]["en"], {})
+        self.assertEqual(response.json()["translations"]["lt"], {})
+
+    def test_regenerate_manifest_creates_site_config_with_authenticated_user(self):
+        url = reverse("regenerate-cache")
+        url_final = reverse("app-config")
+
+        self.query_limits["ANY GET REQUEST"] = 10
+        response = self.authorize().get(url, format="json", HTTP_REFERER=url_final, follow=True)
+        site_config = SiteConfiguration.get_solo()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json()["version"], site_config.manifest_version)
+        self.assertEqual(response.json()["default_language"], settings.DEFAULT_LANGUAGE)
+        self.assertEqual(response.json()["enabled_languages"], [])
+        self.assertEqual(response.json()["translations"]["en"], {})
+        self.assertEqual(response.json()["translations"]["lt"], {})
--- backend/apps/utils/tests/test_send_email_task.py
+++ backend/apps/utils/tests/test_send_email_task.py
@@ -1,40 +1,59 @@
+from django.conf import settings
 from django.core import mail
 from django.test import TestCase
 from django.test.utils import override_settings
+from django.urls import reverse
 from model_bakery.baker import make
+from rest_framework import status
 
 from apps.home.models import EmailTemplate, EmailTemplateTranslation, SiteConfiguration
 from apps.translations.models import Language
+from apps.users.models import User, PasswordKey, ActivationKey
 from apps.utils.tasks import send_email_task
 
 
 @override_settings(DEFAULT_LANGUAGE='en')
 class SendEmailTestCase(TestCase):
+
+    def _add_email_template_and_translation(
+        self,
+        site_config: SiteConfiguration,
+        template_name: str,
+        language: Language,
+        dynamic_content_fragment: str,
+    ) -> None:
+        template, _create = EmailTemplate.objects.get_or_create(name=template_name)
+        setattr(site_config, template_name, template)
+        subject = "Subject"
+        content = f"Follow this link: {{{{ {dynamic_content_fragment} }}}}"
+        make(EmailTemplateTranslation, language=language, template=template, subject=subject, content=content)
+
     def setUp(self):
-        language = Language.objects.create(code='en', name='English')
-        self.template_name = 'password_renewal_template'
-        template = make(EmailTemplate, name=self.template_name)
-        self.subject = 'Password renewal'
-        self.content = 'To reset your password, follow this link: {{ password_url }}.'
-        make(EmailTemplateTranslation, language=language, template=template, subject=self.subject, content=self.content)
-
         site_config = SiteConfiguration.get_solo()
-        site_config.password_renewal_template = template
+        language, _create = Language.objects.get_or_create(code="en", name="English")
         site_config.enabled_languages.add(language)
-        site_config.save()
 
-        self.email = 'test@example.com'
+        self._add_email_template_and_translation(
+            site_config, "password_renewal_template", language, "passwordUrl"
+        )
+        self._add_email_template_and_translation(
+            site_config, "verify_email_template", language, "activationUrl"
+        )
+
+        self.email = "test@example.com"
+        self.user = make(User, email=self.email, language=language)
+        site_config.save()
 
     def test_send_email_task(self):
         password_url = 'http://example.com/api/reset'  # noqa: S105
         send_email_task(
             email=self.email,
-            template=self.template_name,
-            context={'password_url': password_url}
+            template="password_renewal_template",
+            context={'passwordUrl': password_url}
         )
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].to, [self.email])
-        self.assertEqual(mail.outbox[0].subject, self.subject)
+        self.assertEqual(mail.outbox[0].subject, "Subject")
         self.assertIn(password_url, mail.outbox[0].body)
 
     def test_send_email_task_with_cc_and_bcc(self):
@@ -46,12 +65,40 @@ class SendEmailTestCase(TestCase):
             email=self.email,
             cc=cc,
             bcc=bcc,
-            template=self.template_name,
-            context={'password_url': password_url}
+            template="password_renewal_template",
+            context={'passwordUrl': password_url}
         )
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].to, [self.email])
         self.assertEqual(mail.outbox[0].cc, cc)
         self.assertEqual(mail.outbox[0].bcc, bcc)
-        self.assertEqual(mail.outbox[0].subject, self.subject)
+        self.assertEqual(mail.outbox[0].subject, "Subject")
+        self.assertIn(password_url, mail.outbox[0].body)
+
+    def test_resend_verification_email(self):
+        data = {"email": self.email}
+        url = reverse("resend-verification")
+
+        response = self.client.post(url, data=data, format="json")
+        activation_key = ActivationKey.objects.get(user=self.user).activation_key
+        activation_url = settings.VERIFICATION_BASE_URL.format(settings.APP_HOST, activation_key)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].to, [self.email])
+        self.assertEqual(mail.outbox[0].subject, "Subject")
+        self.assertIn(activation_url, mail.outbox[0].body)
+
+    def test_send_password_renewal_email(self):
+        data = {"email": self.email}
+        url = reverse("forgot")
+
+        response = self.client.post(url, data=data, format="json")
+        password_key = PasswordKey.objects.get(user=self.user).password_key
+        password_url = settings.RESET_PASSWORD_BASE_URL.format(settings.APP_HOST, password_key)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].to, [self.email])
+        self.assertEqual(mail.outbox[0].subject, "Subject")
         self.assertIn(password_url, mail.outbox[0].body)
