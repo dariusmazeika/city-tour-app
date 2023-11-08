b814f7a540bd87738500d4b6a312f44b4f0b6c1d
laumak1 <laurynasmaks@gmail.com>
2023-11-08 18:30:32 +0200

    Created /tours/{id}/ enpoint to get tour's information and sights. Wrote enpoint tests.

---
+++ apps/tours/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+from apps.sites.serializers import SiteSerializer
+from apps.tours.models import Tour
+
+
+class TourSerializer(serializers.ModelSerializer):
+    sites = SiteSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Tour
+        fields = "__all__"
---
+++ apps/tours/views.py
@@ -0,0 +1,12 @@
+from rest_framework import mixins
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+
+from apps.tours.models import Tour
+from apps.tours.serializers import TourSerializer
+
+
+class ToursViewSet(viewsets.GenericViewSet, mixins.RetrieveModelMixin):
+    permission_classes = (AllowAny,)
+    queryset = Tour.objects.all()
+    serializer_class = TourSerializer
