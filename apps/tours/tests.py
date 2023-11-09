34f72c1aae99b87ee45a913576904d2fc66c02d0
laumak1 <laurynasmaks@gmail.com>
2023-11-09 16:39:10 +0200

    Fixed comments

--- apps/tours/tests.py
+++ apps/tours/tests.py
@@ -5,13 +5,13 @@ from apps.utils.tests_query_counter import APIClientWithQueryCounter
 
 
 class TestGetTours:
-    def test_get_tours_returns_tours(self, client: APIClientWithQueryCounter, single_tour, expexted_tour_data):
+    def test_get_tours_returns_tours(self, client: APIClientWithQueryCounter, single_tour, expected_tour_data):
         path = reverse("tours-detail", args=[single_tour.id])
         response = client.get(path)
 
         assert response.status_code == status.HTTP_200_OK, response.json()
 
-        assert expexted_tour_data == response.json()
+        assert expected_tour_data == response.json()
 
     def test_get_not_existing_tour_returns_not_found(self, client: APIClientWithQueryCounter):
         not_existing_tour_id = 100
